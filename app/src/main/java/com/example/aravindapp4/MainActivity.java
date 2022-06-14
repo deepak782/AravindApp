cdacf01feca1d41544ea78c522650fb9b8d4d312
deepak782 <deepu.kamal78@gmail.com>
2022-06-14 13:00:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aravindapp4/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.aravindapp4;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST_CODE = 101;
+    private static final int MULTIPLE_REQUEST_CODE =201 ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        findViewById(R.id.single).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              
+                singlePermissionMethod();
+            }
+        });
+        findViewById(R.id.multiple).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+                multiplePermissionMethod();
+
+            }
+        });
+    }
+
+    private void multiplePermissionMethod() {
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS)+
+                ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CALL_LOG)+
+                ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_CONTACTS,
+            Manifest.permission.READ_CALL_LOG,Manifest.permission.READ_EXTERNAL_STORAGE},MULTIPLE_REQUEST_CODE);
+        }
+        else
+        {
+            Toast.makeText(this, "All open Code", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void singlePermissionMethod() {
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},CAMERA_REQUEST_CODE);
+        }
+        else
+        {
+            Toast.makeText(this, "Camera open Code", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode)
+        {
+            case CAMERA_REQUEST_CODE:
+                if(grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED)
+                {
+                    Toast.makeText(this, "Camera open Code", Toast.LENGTH_SHORT).show();
+
+                }
+                else
+                {
+                    Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_SHORT).show();
+
+                }
+                break;
+
+            case MULTIPLE_REQUEST_CODE:
+
+                if(grantResults.length>0&&grantResults[0]+grantResults[1]+grantResults[2]==PackageManager.PERMISSION_GRANTED)
+                {
+                    Toast.makeText(this, "All open Code", Toast.LENGTH_SHORT).show();
+
+                }
+                else
+                {
+                    Toast.makeText(this, "All Permission Denied", Toast.LENGTH_SHORT).show();
+
+                }
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/aravindapp4/MainActivity2.java
@@ -0,0 +1,78 @@
+package com.example.aravindapp4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.List;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        findViewById(R.id.single).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                singlePermissionMethod();
+            }
+        });
+        findViewById(R.id.multiple).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                multiplePermissionMethod();
+
+            }
+        });
+    }
+
+    private void singlePermissionMethod() {
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override public void onPermissionGranted(PermissionGrantedResponse response) {
+                        Toast.makeText(MainActivity2.this, "Granted\n"+response.getPermissionName(), Toast.LENGTH_SHORT).show();}
+                    @Override public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(MainActivity2.this, "Denied\n"+response.getPermissionName(), Toast.LENGTH_SHORT).show();}
+                    @Override public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {/* ... */
+                    token.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+    private void multiplePermissionMethod() {
+
+        Dexter.withContext(this)
+                .withPermissions(
+                        Manifest.permission.READ_CALL_LOG,
+                        Manifest.permission.READ_CONTACTS,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                ).withListener(new MultiplePermissionsListener() {
+            @Override public void onPermissionsChecked(MultiplePermissionsReport report) {
+            report.areAllPermissionsGranted();
+            }
+            @Override public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                token.continuePermissionRequest();
+            }
+        }).check();
+
+    }
+}
